1349e5da9b645981f5f6f53b461a5ec0325cab7c
nazarevrn <kimi-champ@mail.ru>
2021-03-25 16:20:35 +0300

    add buttons st2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textView)
+    }
+
+    fun toastMe(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun countMe (view: View) {
+        // Get the value of the text view.
+        val countString = textView.text.toString()
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+        // Display the new value in the text view.
+        textView.text = count.toString();
     }
 }
